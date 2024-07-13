9a12e2cbd62babe831db6f7632e939adc53c2662
mohit-thakre <mohitthakre010@gmail.com>
2024-07-13 20:16:02 +0530

    day-1

---
+++ DAY1.js
@@ -0,0 +1,26 @@
+var number = 10;
+console.log("number is :", number);
+
+let string = "hello jii";
+console.log("string is :", string);
+
+const boolean = true;
+console.log("boolean is :", boolean);
+
+const array = [1, 2, 3, 4];
+const object = {
+  name: "mohit",
+  age: 20,
+};
+
+console.log("type of number is :", typeof number);
+console.log("type of string is :", typeof string);
+console.log("type of boolean is :", typeof boolean);
+console.log("type of array is :", typeof array);
+console.log("type of object is :", typeof object);
+
+string = "reassigned";
+console.log("reassigned value of let (string) :", string);
+
+boolean = false;
+console.log(boolean);
