357ba8cf3ff49540efb93bcfb14ff7160ffa04f6
mohit-thakre <mohitthakre010@gmail.com>
2024-07-16 21:58:43 +0530

    day-4

---
+++ DAY4.js
@@ -0,0 +1,63 @@
+// ACVTIVITY 1
+// TASK 1
+
+for (let a = 1; a <= 10; a++) {
+  console.log(a);
+}
+
+// TASK 2
+
+for (let b = 1; b <= 10; b++) {
+  console.log("5 *", b, "=", 5 * b);
+}
+
+// ACTIVITY 2
+
+//TASK 3
+
+let c = 1;
+let ans = 0;
+while (c <= 10) {
+  ans += c;
+  c++;
+}
+console.log(ans);
+
+// TASK 4
+
+let d = 10;
+while (d >= 1) {
+  console.log(d);
+  d--;
+}
+
+// ACTIVITY 3
+
+//TASK 5
+
+let e = 1;
+
+do {
+  console.log(e);
+  e++;
+} while (e <= 5);
+
+// TASK 6
+
+let number = 10;
+let factorial = 1;
+do {
+  factorial *= number;
+  number--;
+} while (number >= 1);
+console.log(factorial);
+
+// ACTIVITY 4
+// TASK 6
+
+// for (let i = 0; i <= 5; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     console.log("*s");
+//   }
+//   console.log(" ");
+// }
